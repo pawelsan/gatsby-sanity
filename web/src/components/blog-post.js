2f865b18f06f6378274b0a1a050cd735ade77b65
pawelsan <pawelsan@yahoo.com>
2020-06-26 15:55:23 +0200

    customisation of components

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -1,15 +1,14 @@
-import {format, distanceInWords, differenceInDays} from 'date-fns'
+import { format, distanceInWords, differenceInDays } from 'date-fns'
 import React from 'react'
-import {buildImageObj} from '../lib/helpers'
-import {imageUrlFor} from '../lib/image-url'
+import { buildImageObj } from '../lib/helpers'
+import { imageUrlFor } from '../lib/image-url'
 import PortableText from './portableText'
 import Container from './container'
-import AuthorList from './author-list'
 
 import styles from './blog-post.module.css'
 
-function BlogPost (props) {
-  const {_rawBody, authors, categories, title, mainImage, publishedAt} = props
+function BlogPost(props) {
+  const { _rawBody, title, mainImage, publishedAt } = props
   return (
     <article className={styles.root}>
       {mainImage && mainImage.asset && (
@@ -37,17 +36,6 @@ function BlogPost (props) {
                 {differenceInDays(new Date(publishedAt), new Date()) > 3
                   ? distanceInWords(new Date(publishedAt), new Date())
                   : format(new Date(publishedAt), 'MMMM Do, YYYY')}
-              </div>
-            )}
-            {authors && <AuthorList items={authors} title='Authors' />}
-            {categories && (
-              <div className={styles.categories}>
-                <h3 className={styles.categoriesHeadline}>Categories</h3>
-                <ul>
-                  {categories.map(category => (
-                    <li key={category._id}>{category.title}</li>
-                  ))}
-                </ul>
               </div>
             )}
           </aside>
