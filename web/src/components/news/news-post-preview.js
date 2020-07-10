584e3ba8b8785bf1a5959b02554f6384f9df0012
pawelsan <pawelsan@yahoo.com>
2020-07-10 15:29:06 +0200

    change post previews css

--- web/src/components/news/news-post-preview.js
+++ web/src/components/news/news-post-preview.js
@@ -17,23 +17,23 @@ function NewsPostPreview(props) {
       <div className={styles.post}>
         <h3 className={styles.post_title}>{props.title}</h3>
         <div className={styles.post_date}>{format(props.publishedAt, 'DD.MM.YYYY')}</div>
-        <div>
+        <div className={styles.post_content}>
           {props.mainImage && props.mainImage.asset && (
+
             <img
+              className={styles.post_image}
               src={imageUrlFor(buildImageObj(props.mainImage))
-                .width(100)
-                .height(100)
+                .width(80)
+                .height(80)
                 .auto('format')
                 .url()}
               alt={props.mainImage.alt}
             />
           )}
-        </div>
-        {props._rawExcerpt && (
-          <div className={styles.post_content}>
+          {props._rawExcerpt && (
             <PortableText blocks={props._rawExcerpt} />
-          </div>
-        )}
+          )}
+        </div>
       </div>
 
 
