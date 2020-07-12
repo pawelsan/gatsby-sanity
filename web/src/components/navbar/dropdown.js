e18d3ce790d2200052e81bb17853a65b66a73a57
pawelsan <pawelsan@yahoo.com>
2020-07-12 23:51:14 +0200

    change style switching conditions in Dropdown

--- web/src/components/navbar/dropdown.js
+++ web/src/components/navbar/dropdown.js
@@ -6,20 +6,20 @@ import styles from './navbar.module.css'
 const Dropdown = ({ subSiteTitle }) => {
     const [openDropdown, setOpenDropdown] = useState(false);
 
-    let active
-    if (subSiteTitle === "Informacje ogólne" || subSiteTitle === "Rodziny zastępcze" || subSiteTitle === "Osoby z niepełnosprawnością" || subSiteTitle === "Pomoc instytucjonalna") {
-        active = true
+    // let active
+    // if (subSiteTitle === "Informacje ogólne" || subSiteTitle === "Rodziny zastępcze" || subSiteTitle === "Osoby z niepełnosprawnością" || subSiteTitle === "Pomoc instytucjonalna") {
+    //     active = true
 
-    } else {
-        active = false
-    }
+    // } else {
+    //     active = false
+    // }
     return (
         <div className={styles.nav_item} onClick={() => setOpenDropdown(!openDropdown)}>
             <div className={
-                active && subSiteTitle === "Piecza zastępcza" ? `${styles.nav_item_dropdown} ${styles.active_blue}` :
-                    active && (subSiteTitle === "Rehabilitacja społeczna" || subSiteTitle === "Aktywny samorząd") ? `${styles.nav_item_dropdown} ${styles.active_green}` :
-                        active && (subSiteTitle === "Domy pomocy społecznej" || subSiteTitle === "Środowiskowe domy samopomocy" || subSiteTitle === "Warsztaty terapii zajęciowej") ? `${styles.nav_item_dropdown} ${styles.active_orange}` :
-                            active ? `${styles.nav_item_dropdown} ${styles.active}` : styles.nav_item_dropdown
+                subSiteTitle === "Piecza zastępcza" ? `${styles.nav_item_dropdown} ${styles.active_blue}` :
+                    subSiteTitle === "Rehabilitacja społeczna" || subSiteTitle === "Aktywny samorząd" ? `${styles.nav_item_dropdown} ${styles.active_green}` :
+                        subSiteTitle === "Domy Pomocy Społecznej" || subSiteTitle === "Środowiskowe Domy Samopomocy" || subSiteTitle === "Warsztaty Terapii Zajęciowej" ? `${styles.nav_item_dropdown} ${styles.active_orange}` :
+                            subSiteTitle === "Informacje ogólne" || subSiteTitle === "Cudzoziemcy" ? `${styles.nav_item_dropdown} ${styles.active}` : styles.nav_item_dropdown
             }>
                 <div>
                     <div>Nasza</div>
