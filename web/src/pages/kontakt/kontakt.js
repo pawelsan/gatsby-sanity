06f4cb77c0fb82bc10243082ace228911a2826c2
pawelsan <pawelsan@yahoo.com>
2020-07-24 23:29:58 +0200

    add Contact details

--- web/src/pages/kontakt/kontakt.js
+++ web/src/pages/kontakt/kontakt.js
@@ -1,14 +1,54 @@
 import React from 'react'
-
+import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
 
-const Contact = () => (
-    <Layout>
-        <SEO title='Kontakt' />
-        <h1>Skontaktuj się z nami</h1>
-        <p>Formularz kontaktowy</p>
-    </Layout>
-)
+import PortableText from '../../components/portableText'
+
+import styles from '../pages.module.css'
+
+export const query = graphql`
+
+query ContactQuery {
+    contact: sanityPageContent(pageName: {eq: "Dane Kontaktowe"}) {
+        id
+        pageName
+        contentTitle
+        _rawContent
+      }
+}
+`
+
+const Contact = props => {
+    const { data, errors } = props
+
+    if (errors) {
+        return (
+            <Layout>
+                <GraphQLErrorList errors={errors} />
+            </Layout>
+        )
+    }
+
+    const contact = (data || {}).contact
+
+    const { _rawContent, contentTitle, pageName } = contact
+
+    if (!contact) {
+        throw new Error(
+            'Missing "Koordynatorzy". Open the studio at http://localhost:3333 and add some content to "Koordynatorzy" and restart the development server.'
+        )
+    }
+
+    return (
+        <Layout>
+            <SEO title='Kontakt' />
+            <h1>{contentTitle}</h1>
+            <div className={styles.gdpr}>
+                {_rawContent && <PortableText blocks={_rawContent} />}
+            </div>
+        </Layout>
+    )
+}
 
 export default Contact
