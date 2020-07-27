15e3ef1eee2fd9d45b361c6ac92978a317e3ec80
pawelsan <pawelsan@yahoo.com>
2020-07-27 21:20:03 +0200

    add inline img functionality  with float

--- web/src/components/portableText.js
+++ web/src/components/portableText.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import clientConfig from '../../client-config'
 import BasePortableText from '@sanity/block-content-to-react'
+import { imageUrlFor } from '../lib/image-url'
 
-const PortableText = ({ blocks }) => (
-  <BasePortableText blocks={blocks} {...clientConfig.sanity} />
-)
+const serializer = {
+  types: {
+    mainImage: props => {
+      console.log(props)
+      return (
+        <figure>
+          <img src={imageUrlFor(props.node.asset).width(200).url()} alt={props.node.all} />
+          <figcaption>{props.node.caption}</figcaption>
+        </figure>
+      )
+    }
+  }
+}
+
+const PortableText = ({ blocks }) => {
+  console.log(blocks)
+  return (
+    <BasePortableText blocks={blocks} {...clientConfig.sanity} serializers={serializer} />
+  )
+}
+
+// const PortableText = ({ blocks }) => (
+//   <BasePortableText blocks={blocks} {...clientConfig.sanity} />
+// )
 
 export default PortableText
