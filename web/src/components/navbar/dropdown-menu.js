30384ed13747a610f7e4c7d156dda9354b1cf556
pawelsan <pawelsan@yahoo.com>
2020-11-28 00:11:32 +0100

    add rough new display of dropdown

--- web/src/components/navbar/dropdown-menu.js
+++ web/src/components/navbar/dropdown-menu.js
@@ -1,20 +1,28 @@
 import { Link } from 'gatsby';
 import React from 'react';
-import Dropdown from './dropdown'
 import styles from './navbar.module.css'
 
-const DropdownMenu = ({ items, primary }) => {
+const DropdownMenu = ({ items }) => {
 
     return (
-        <div className={primary ? `${styles.dropdown} ${styles.primary_dropdown}` : styles.dropdown}>
+        <div className={styles.dropdown}>
             {items.map((item, index) =>
-                item.dropdown ? <Dropdown key={index} text={item.text} items={item.dropdownItems} color={item.color} primary={false} /> :
-                    <Link key={index} className={`${styles.nav_item} ${styles[`nav_item_${item.color}`]}`} activeClassName={`${styles.active} ${styles[`active_item_${item.color}`]}`} to={item.to}>{item.text}</Link>
+                !item.to
+                    ?
+                    <div>
+                        <span>{item.text}</span>
+                        {item.subItems.map((subItem, index) =>
+                            <Link key={index}
+                                className={`${styles.dropdown_item} ${styles[`dropdown_item_${subItem.color}`]}`}
+                                activeClassName={`${styles.active} ${styles[`active_item_${subItem.color}`]}`}
+                                to={subItem.to}>{subItem.text}</Link>)}
+                    </div>
+                    :
+                    <Link key={index}
+                        className={`${styles.dropdown_item} ${styles[`dropdown_item_${item.color}`]}`}
+                        activeClassName={`${styles.active} ${styles[`active_item_${item.color}`]}`}
+                        to={item.to}>{item.text}</Link>
             )}
-
-            {/* <Link className={styles.drop_item} to></Link>
-            <Link className={styles.drop_item} to></Link>
-            <Link className={styles.drop_item} to></Link> */}
         </div>
     )
 };
--- web/src/components/navbar/dropdown.js
+++ web/src/components/navbar/dropdown.js
@@ -1,61 +1,61 @@
-import React, { useState, useEffect } from 'react'
-import DropdownMenu from './dropdown-menu';
+// import React, { useState, useEffect } from 'react'
+// import DropdownMenu from './dropdown-menu';
 
-import styles from './navbar.module.css'
+// import styles from './navbar.module.css'
 
-const Dropdown = ({ items, text, color, primary }) => {
+// const Dropdown = ({ items, text, color, primary }) => {
 
-    const [openDropdown, setOpenDropdown] = useState(false);
-    const [activeItem, setActiveItem] = useState();
-    const [activeItemInNestedDropdown, setActiveItemInNestedDropdown] = useState();
+//     const [openDropdown, setOpenDropdown] = useState(false);
+//     const [activeItem, setActiveItem] = useState();
+//     const [activeItemInNestedDropdown, setActiveItemInNestedDropdown] = useState();
 
-    useEffect(() => {
-        setActiveItem(items.find(item => item.to === location.pathname)),
-            setActiveItemInNestedDropdown(items
-                .filter(item => item.dropdown === true)
-                .find(item => item.dropdownItems
-                    .find(item => item.to === location.pathname)))
-    }
+//     useEffect(() => {
+//         setActiveItem(items.find(item => item.to === location.pathname)),
+//             setActiveItemInNestedDropdown(items
+//                 .filter(item => item.dropdown === true)
+//                 .find(item => item.dropdownItems
+//                     .find(item => item.to === location.pathname)))
+//     }
 
-        , []);
+//         , []);
 
-    // const activeItem = items.find(item => item.to === location.pathname);
+//     // const activeItem = items.find(item => item.to === location.pathname);
 
-    // const activeItemInNestedDropdown = items
-    //     .filter(item => item.dropdown === true)
-    //     .find(item => item.dropdownItems
-    //         .find(item => item.to === location.pathname))
+//     // const activeItemInNestedDropdown = items
+//     //     .filter(item => item.dropdown === true)
+//     //     .find(item => item.dropdownItems
+//     //         .find(item => item.to === location.pathname))
 
-    let activeDropdown
+//     let activeDropdown
 
-    let dropdownColor
+//     let dropdownColor
 
-    // check if an active link is included in a given dropdown
-    if (activeItem) {
-        activeDropdown = true
-        dropdownColor = activeItem.color
-        // check if an active link is included in a given dropdown's nested dropdown
-    } else if (activeItemInNestedDropdown) {
-        activeDropdown = true
-        dropdownColor = activeItemInNestedDropdown.color
-    } else {
-        activeDropdown = false
-        dropdownColor = color
-    }
+//     // check if an active link is included in a given dropdown
+//     if (activeItem) {
+//         activeDropdown = true
+//         dropdownColor = activeItem.color
+//         // check if an active link is included in a given dropdown's nested dropdown
+//     } else if (activeItemInNestedDropdown) {
+//         activeDropdown = true
+//         dropdownColor = activeItemInNestedDropdown.color
+//     } else {
+//         activeDropdown = false
+//         dropdownColor = color
+//     }
 
-    return (
-        <div
-            className={activeDropdown ? `${styles.nav_item} ${styles.active} ${styles[`active_item_${dropdownColor}`]}` : `${styles.nav_item} ${styles[`nav_item_${dropdownColor}`]}`}
-            onMouseEnter={() => setOpenDropdown(true)}
-            onMouseLeave={() => setOpenDropdown(false)}>
+//     return (
+//         <div
+//             className={activeDropdown ? `${styles.nav_item} ${styles.active} ${styles[`active_item_${dropdownColor}`]}` : `${styles.nav_item} ${styles[`nav_item_${dropdownColor}`]}`}
+//             onMouseEnter={() => setOpenDropdown(true)}
+//             onMouseLeave={() => setOpenDropdown(false)}>
 
-            <div>{text}</div>
+//             <div>{text}</div>
 
-            {/* <div className={openDropdown ? `${styles.arrow} ${styles.arrow_up}` : styles.arrow}></div> */}
+//             {/* <div className={openDropdown ? `${styles.arrow} ${styles.arrow_up}` : styles.arrow}></div> */}
 
-            {openDropdown && <DropdownMenu items={items} primary={primary} />}
-        </div>
-    );
-}
+//             {openDropdown && <DropdownMenu items={items} primary={primary} />}
+//         </div>
+//     );
+// }
 
-export default Dropdown;
+// export default Dropdown;
