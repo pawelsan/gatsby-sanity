130ec96ba1130b92e63e84ed627a794269359151
pawelsan <pawelsan@yahoo.com>
2021-01-27 21:36:09 +0100

    fix bugs in header and dark mode logic and styles

--- web/src/components/header/components/home.js
+++ web/src/components/header/components/home.js
@@ -4,11 +4,11 @@ import logo from "../../../images/logo-small.png";
 import styles from "../styles/header.module.css";
 
 
-const Home = ({ title, smallViewport, handleShowNav }) => {
+const Home = ({ title, handleShowNav }) => {
     return (
         <li className={`${styles.nav_item} ${styles.home}`} onClick={handleShowNav}>
             <Link to="/" className={styles.home_link} activeClassName={styles.nav_item_active}>
-                {!smallViewport && <img className={styles.logo} src={logo} alt="Logo PCPR" />}
+                <img className={styles.home_logo} src={logo} alt="Logo PCPR" />
                 <span className={styles.home_span}> {title}</span>
             </Link>
         </li>
--- web/src/components/header/header.js
+++ web/src/components/header/header.js
@@ -35,33 +35,18 @@ const Header = () => {
   data.allSanityAboutPcpr.edges.map((item) => aboutDropdownItems.push(item.node));
   aboutDropdownItems = sortNavItemsByOrder(aboutDropdownItems, "order");
 
-  // states
   const [navIsShown, setNavIsShown] = useState(false);
-  const [viewportWidth, setViewportWidth] = useState(
-    Math.max(typeof window !== `undefined` ? window.document.documentElement.clientWidth : null)
-  );
 
-  // handlers
   const handleShowNav = () => {
-    smallViewport && setNavIsShown(!navIsShown);
+    setNavIsShown(!navIsShown);
   };
-  const handleViewportWidth = () => {
-    setViewportWidth(Math.max(typeof window !== `undefined` ? window.document.documentElement.clientWidth : null));
-  };
-
-  if (typeof window !== `undefined`) {
-    window.onresize = handleViewportWidth;
-    window.onload = handleViewportWidth;;
-  }
-
-  const smallViewport = viewportWidth < 768 ? true : false
 
   return (
     <header>
       <div className={styles.header_container}>
-        {smallViewport && <Toggler handleShowNav={handleShowNav} navIsShown={navIsShown} />}
+        <Toggler handleShowNav={handleShowNav} navIsShown={navIsShown} />
         <Navbar navIsShown={navIsShown} >
-          <Home title={navItems.main} smallViewport={smallViewport} handleShowNav={handleShowNav} />
+          <Home title={navItems.main} handleShowNav={handleShowNav} />
           <DropdownParent title={navItems.about}>
             <DropdownPanel items={aboutDropdownItems} />
           </DropdownParent>
@@ -78,13 +63,11 @@ const Header = () => {
             <DropdownPanel items={null} />
           </DropdownParent>
         </Navbar>
-        {smallViewport && (
-          <div className={styles.logo_wrapper}>
-            <Link to="/">
-              <img className={styles.logo} src={logo} alt="Logo PCPR" />
-            </Link>
-          </div>
-        )}
+        <div className={styles.logo_wrapper}>
+          <Link to="/">
+            <img className={styles.logo} src={logo} alt="Logo PCPR" />
+          </Link>
+        </div>
         <div className={styles.rightside_wrapper}>
           <a href="http://www.pcprzyrardow.naszbip.pl/">
             <img src={bip} alt="Logo bip" />
