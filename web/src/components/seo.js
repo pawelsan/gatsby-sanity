b8714e37a58fea952e9b2c86aad664e93279c8b6
pawelsan <pawelsan@yahoo.com>
2021-02-08 21:36:37 +0100

    SEO file modification

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -1,23 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import {StaticQuery, graphql} from 'gatsby'
-import {imageUrlFor} from '../lib/image-url'
-import {buildImageObj} from '../lib/helpers'
+import { StaticQuery, graphql } from 'gatsby'
 
-function SEO ({description, lang, meta, keywords, title, image}) {
+function SEO({ description, keywords, title }) {
   return (
     <StaticQuery
       query={detailsQuery}
       render={data => {
         const metaDescription = description || (data.site && data.site.description) || ''
         const siteTitle = (data.site && data.site.title) || ''
-        const siteAuthor = (data.site && data.site.author && data.site.author.name) || ''
-        const metaImage = (image && image.asset) ? imageUrlFor(buildImageObj(image)).width(1200).url() : ''
 
         return (
           <Helmet
-            htmlAttributes={{lang}}
             title={title}
             titleTemplate={title === siteTitle ? '%s' : `%s | ${siteTitle}`}
             meta={[
@@ -38,16 +33,8 @@ function SEO ({description, lang, meta, keywords, title, image}) {
                 content: 'website'
               },
               {
-                property: 'og:image',
-                content: metaImage
-              },
-              {
                 name: 'twitter:card',
                 content: 'summary'
-              },
-              {
-                name: 'twitter:creator',
-                content: siteAuthor
               },
               {
                 name: 'twitter:title',
@@ -65,8 +52,7 @@ function SEO ({description, lang, meta, keywords, title, image}) {
                     content: keywords.join(', ')
                   }
                   : []
-              )
-              .concat(meta)}
+              )}
           />
         )
       }}
@@ -75,15 +61,11 @@ function SEO ({description, lang, meta, keywords, title, image}) {
 }
 
 SEO.defaultProps = {
-  lang: 'en',
-  meta: [],
   keywords: []
 }
 
 SEO.propTypes = {
   description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.array,
   keywords: PropTypes.arrayOf(PropTypes.string),
   title: PropTypes.string.isRequired
 }
